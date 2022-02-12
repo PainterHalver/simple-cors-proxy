4849dfecee6eeee0837bd42cbdeb9f3780a5bec7
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-02-12 20:13:39 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const axios = require("axios");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+app.options("*", cors());
+axios.defaults.timeout = 5000;
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, function () {
+  console.log("Proxy server listening on port " + port);
+});
+
+// https://calputer.herokuapp.com/api/word/感じる
+app.all("/*", async function (req, res, next) {
+  try {
+    if (!req.url) {
+      res.status(500).send({
+        error: "There is no url request params",
+      });
+      return;
+    }
+    const url = req.url.slice(1);
+    console.log(url);
+    const response = await axios({
+      url: url,
+      method: req.method,
+      data: req.body,
+      headers: { "Content-Type": "application/json" },
+    });
+    const { data } = response;
+    res.status(200).json(data);
+  } catch (error) {
+    if (error.errno === -4078) {
+      res.status(500).json({
+        error,
+      });
+    } else {
+      res.status(418).json({
+        me: "?? A tea pot ??",
+        error,
+      });
+    }
+  }
+});
+
+app.get("*", (req, res, next) => {
+  res.status(200).json({
+    data: "This route is not defined!",
+  });
+});
